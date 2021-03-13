0091b4ac0e5a653be4ca57d84d09fda8ad3731f4
rodneirpo <rodneirpo@yahoo.com.br>
2021-03-12 21:34:25 -0300

    teste1

--- SRC/routes/index.js
+++ SRC/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get('/api', (req, res) => {
   res.status(200).send({
     success: 'true',
-    message: 'GLOBAL ORG',
+    message: 'TESTE RODNEI',
     version: '1.0.0',
   });
 });
